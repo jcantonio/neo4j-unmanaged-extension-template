8d680d950245985f20162d16b0769faf31a81025
Max De Marzi <maxdemarzi@hotmail.com>
2012-11-15 04:41:23 -0600

    trying a few things

--- src/main/java/org/neo4j/example/unmanagedextension/MyService.java
+++ src/main/java/org/neo4j/example/unmanagedextension/MyService.java
@@ -15,6 +15,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
+import java.util.HashMap;
+
+import org.neo4j.helpers.collection.IteratorUtil;
+import org.neo4j.graphdb.Node;
+import org.neo4j.graphdb.Relationship;
+import org.neo4j.graphalgo.*;
+import org.neo4j.kernel.Traversal;
+import org.neo4j.tooling.GlobalGraphOperations;
+import org.neo4j.server.rest.repr.PathRepresentation;
+import org.neo4j.server.rest.repr.WeightedPathRepresentation;
+import javax.ws.rs.Produces;
 
 @Path("/service")
 public class MyService {
@@ -24,6 +35,89 @@ public class MyService {
     public String helloWorld() {
         return "Hello World!";
     }
+
+    @GET
+    @Path("/warmup")
+    public String warmUp(@Context GraphDatabaseService db) {
+		for ( Node node : db.getAllNodes() ) {
+		   IteratorUtil.count( node.getRelationships() );
+		 }
+	    return "Warmed up and ready to go!";
+    }
+
+    @GET
+    @Path("/warmup2")
+    public String warmUp2(@Context GraphDatabaseService db) {
+		Node start;
+		for ( Node n : GlobalGraphOperations.at( db ).getAllNodes() ) {
+		   n.getPropertyKeys();
+		   for ( Relationship relationship : n.getRelationships() ) {
+              start = relationship.getStartNode();
+           }
+        }
+        for ( Relationship r : GlobalGraphOperations.at( db ).getAllRelationships() ) {
+          start = r.getStartNode();
+        }
+    return "Warmed up and ready to go too!";
+    }
+
+
+    @GET
+    @Path("/astar/{from}/{to}")
+    @Produces("application/json")
+    public Response routeAStar(@PathParam("from") Long from, @PathParam("to") Long to, @Context GraphDatabaseService db) throws IOException {
+       Node nodeA = db.getNodeById(from);
+       Node nodeB = db.getNodeById(to);
+
+  	   EstimateEvaluator<Double> estimateEvaluator = new EstimateEvaluator<Double>()
+	        {
+	            public Double getCost( final Node node, final Node goal )
+	            {
+	                double dx = (Double) node.getProperty( "x" ) - (Double) goal.getProperty( "x" );
+	                double dy = (Double) node.getProperty( "y" ) - (Double) goal.getProperty( "y" );
+	                double result = Math.sqrt( Math.pow( dx, 2 ) + Math.pow( dy, 2 ) );
+	                return result;
+	            }
+	        };
+	        PathFinder<WeightedPath> astar = GraphAlgoFactory.aStar(
+	                Traversal.expanderForAllTypes(),
+	                CommonEvaluators.doubleCostEvaluator( "time" ), estimateEvaluator );
+	        WeightedPath path = astar.findSinglePath( nodeA, nodeB );
+	
+			Map<String, Object> astarMap = new HashMap<String, Object>();
+			astarMap.put("time", path.weight());
+			
+			List<Object> nodes = new ArrayList<Object>();
+			for ( Node node : path.nodes() )
+			    {
+			      nodes.add(node.getId());
+	            }
+			astarMap.put("nodes", nodes);
+			
+			List<Object> relationships = new ArrayList<Object>();
+			for ( Relationship relationship : path.relationships() )
+			    {
+            		 Map<String, Object> relMap = new HashMap<String, Object>();
+                     relMap.put("id", relationship.getId());
+                     relMap.put("rel_type", relationship.getType().name());
+                     relMap.put("start_node", relationship.getStartNode().getId());
+                     relMap.put("end_node", relationship.getEndNode().getId());
+                     relMap.put("time", relationship.getProperty("time"));
+			         relationships.add(relMap);
+	            }
+
+			astarMap.put("relationships", relationships);
+            
+            //PathRepresentationCreator representationCreator = WEIGHTED_PATH_REPRESENTATION_CREATOR;
+
+			ObjectMapper objectMapper = new ObjectMapper();
+	        //return Response.ok().entity(objectMapper.writeValueAsString(path.toString())).build();
+	        //return Response.ok().entity(WeightedPathRepresentation(path)).build();
+            //return Response.ok().entity(new WeightedPathRepresentation( path )).build();
+            //return Response.ok().entity(new PathRepresentation( path ).toString()).build();
+            //return Response.ok().entity(objectMapper.writeValueAsString(astarMap)).build();
+            return Response.ok().entity(objectMapper.writeValueAsString(new WeightedPathRepresentation( path ))).build();
+}
 
     @GET
     @Path("/friends/{name}")
